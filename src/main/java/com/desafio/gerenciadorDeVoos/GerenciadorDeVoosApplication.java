c923fc25f5fb9386ba4a6e3de534177992b78cc1
TansenSilva <tansensilva@gmail.com>
2022-11-21 09:57:10 -0300

    first commit

---
+++ src/main/java/com/desafio/gerenciadorDeVoos/GerenciadorDeVoosApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.gerenciadorDeVoos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeVoosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeVoosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/gerenciadorDeVoos/GerenciadorDeVoosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.gerenciadorDeVoos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeVoosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
